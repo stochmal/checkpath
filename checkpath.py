ab17cca971aec3f4ef1bf67f05ede6f26b334a1c
Tomasz Stochmal <stochmal@gmail.com>
2021-08-30 20:30:23 +0200

    pylint fixes

--- checkpath.py
+++ checkpath.py
@@ -1,28 +1,29 @@
+'''main module'''
 import os
 
 
-from pprint import pprint
-
-COPYRIGHT = 'Copyright (C) 2021 Tomasz Stochmal <stochmal@gmail.com>\nhttps://github.com/stochmal/checkpath/blob/main/LICENSE\n'
+COPYRIGHT = 'Copyright (C) 2021 Tomasz Stochmal <stochmal@gmail.com>'
+LICENSE = 'https://github.com/stochmal/checkpath/blob/main/LICENSE'
 
 def main():
+    '''main functiom'''
+    print(COPYRIGHT)
+    print(LICENSE)
+    print()
 
-        print(COPYRIGHT)
+    print('--- folders that can be removed from PATH ---')
 
-        print('--- folders that can be removed from PATH ---')
+    path = os.environ['PATH']
+    folders = path.split(';')
+    for folder in folders:
+        exist = os.path.exists(folder)
 
-        path = os.environ['PATH']
-        folders = path.split(';')
-        for folder in folders:
-            exist = os.path.exists(folder)
-
-            if not exist:
-                print(folder, '\t*** FOLDER NOT FOUND ***')
-            else:
-                files = os.listdir(folder)
-                if(len(files)==0):
-                    print(folder, '\t*** FOLDER IS EMPTY ***')
-
+        if not exist:
+            print(folder, '\t*** FOLDER NOT FOUND ***')
+        else:
+            files = os.listdir(folder)
+            if len(files)==0:
+                print(folder, '\t*** FOLDER IS EMPTY ***')
 
 if __name__=='__main__':
     main()
